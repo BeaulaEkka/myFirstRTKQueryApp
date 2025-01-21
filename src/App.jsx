663fc8d124d9d23311e0522936850aa32e89d685
Your Name <ekka.beaula@gmail.com>
2025-01-21 23:56:47 +0100

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 
 import './App.css'
+import { useGetPostQuery } from './services/jsonPlaceholderApi'
+
+
 
 function App() {
-  
+  const {data,error,isLoading}=useGetPostQuery()
 
+  if(isLoading)return <p>loading...</p>;
+
+  if(error) return <p>There was an error:</p>
+  console.log(data)
   return (
     <>
       hello RTK homepage
