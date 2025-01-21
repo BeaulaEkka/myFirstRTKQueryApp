9728e6fe7fe87ac340758361d5aa047318bb3670
Your Name <ekka.beaula@gmail.com>
2025-01-21 23:46:56 +0100

    middlewares

--- src/services/jsonPlaceholderApi.js
+++ src/services/jsonPlaceholderApi.js
@@ -1,1 +1,21 @@
-export const jsonPlaceholderApi = () => {};
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+export const jsonPlaceholderApi = createApi({
+  reducerPath: "jsonPlaceholderApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://jsonplaceholder.typicode.com/",
+  }),
+  endpoints: (builder) => ({
+    //Query and fetch posts
+    getPosts: builder.query({ query: () => "posts" }),
+    createPosts: builder.mutation({
+      query: (newPost) => ({
+        url: "posts",
+        method: "POST",
+        body: newPost,
+      }),
+    }),
+  }),
+});
+
+export const { useGetPostsQuery, useCreatePostsMutation } = jsonPlaceholderApi;
--- src/store.js
+++ src/store.js
@@ -1,5 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { jsonPlaceholderApi } from "./services/jsonPlaceholderApi";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    // reducers
+    [jsonPlaceholderApi.reducerPath]: jsonPlaceholderApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(jsonPlaceholderApi.middleware),
 });
+
+//optional but has nice features like refresh
+setupListeners(store.dispatch);
